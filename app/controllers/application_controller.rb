c85e9c29369f11bc87343e5bb6a869fe1eef387f
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-08-01 15:33:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/results' do
+    @quiz= Dog.new(params[:name], params[:q1].to_i, params[:q2].to_i, params[:q3].to_i)
+    
+    "Your breed is a #{@quiz.results}!"
+    
   end
 
 end
